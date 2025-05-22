42c5a0abf1b47bcdd3efc2a60625f1e84c46beef
FrancoFerrandi <franco.ferrandi@gmail.com>
2025-05-21 21:31:11 -0300

    First Commit

---
+++ Par_1C124.21052025.FerrandiFranco/src/par_1c124/pkg21052025/ferrandifranco/Par_1C12421052025FerrandiFranco.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package par_1c124.pkg21052025.ferrandifranco;
+
+/**
+ *
+ * @author el_fr
+ */
+public class Par_1C12421052025FerrandiFranco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
